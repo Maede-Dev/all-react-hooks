7e894363726e6c6cd7f286a3af3ff53f2fa9a086
karimi <m.karimi@sebyan.local>
2022-12-26 15:13:16 +0330

    handle useState

--- src/pages/components/useState/UseState.js
+++ src/pages/components/useState/UseState.js
@@ -4,14 +4,28 @@ import s from "./style.module.scss";
 const UseState = () => {
   const [counter, setCounter] = useState(0);
 
-  const increment = () => {
-    setCounter(counter + 1);
-  };
+  // const increment = () => {
+  //   setCounter(counter + 1);
+  // };
+
+  // const decrement = () => {
+  //   if (counter >= 1) {
+  //     setCounter(counter - 1);
+  //   }
+  // };
+
   return (
     <div className={s.container}>
-      {counter}
-      <button onClick={increment} className={s.btn}>
+      <button onClick={() => setCounter(counter + 1)} className={s.btn}>
         +
+      </button>
+      {counter}
+      <button
+        onClick={() => setCounter(counter - 1)}
+        className={s.btn}
+        disabled={counter <= 1}
+      >
+        -
       </button>
     </div>
   );
