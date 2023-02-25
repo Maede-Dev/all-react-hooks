a41fa847b73c1917962a6b8914bb20c0d0e2dfb1
m.karimi <maedekarimi69110@gmail.com>
2023-02-25 14:54:40 +0330

    handle useRef comment

--- src/pages/components/UseRef/RefTutorial.js
+++ src/pages/components/UseRef/RefTutorial.js
@@ -2,7 +2,7 @@ import { useRef } from "react";
 import s from "./style.module.scss";
 
 //baraye in ke ro dokme click miknim harja mouse bashe miad focus mishe ro input va amade type va input ro clear kne bade type
-
+//dastresi be elementhaye safhe(DOM)
 const RefTutorial = () => {
   const inputRef = useRef(null);
 
