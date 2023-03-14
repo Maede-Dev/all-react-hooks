220ca8ce3a1fad209d20188a5d37d449db516be8
maede <maedekarimi69110@gmail.com>
2023-03-14 13:23:12 +0330

    handle comments

--- src/pages/components/UseImperativeHandle/UseImperativeHandle.js
+++ src/pages/components/UseImperativeHandle/UseImperativeHandle.js
@@ -2,8 +2,7 @@ import { useRef } from "react";
 import ChildUseRef from "./components/childUseRef";
 import s from "./style.module.scss";
 
-//baraye in ke ro dokme click miknim harja mouse bashe miad focus mishe ro input va amade type va input ro clear kne bade type
-//dastresi be elementhaye safhe(DOM)
+//dstresi be child az parent
 const UseImperativeHandle = () => {
   const buttonRef = useRef(null);
   return (
